ff03ca9341ce75fa34ce1f0df1b9583c12d1a851
wsj <1752586533@qq.com>
2021-03-27 10:45:20 +0800

    Github测试

---
+++ test_1_20_hanshu/test_1_20_hanshu/test.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+//求字符串长度
+//int my_strlen(char* p)
+//{
+//	int count = 0;
+//	while (*p != '\0')
+//	{
+//		count++;
+//		p++;
+//	}
+//	return count;
+//}
+
+//函数递归方法
+//int my_strlen(char* p)
+//{
+//	if (*p != '\0')
+//		return 1 + my_strlen(p+1);
+//	else
+//		return 0;
+//}
+//
+//int main()
+//{
+//	char arr[] = "wsj111";
+//	//调用求字符串长度函数
+//	//int len = strlen(arr);
+//	int len = my_strlen(arr);
+//	printf("len = %d",len);
+//	return 0;
+//}
+
